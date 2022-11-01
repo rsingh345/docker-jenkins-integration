f91339f79642e205fac7938cb7cabb5cb8313a4b
rakesh singh <srkmca07@gmail.com>
2022-11-01 22:56:32 +0530

    changes are done in main java class

--- src/main/java/com/dedalus/spring/apis/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/dedalus/spring/apis/DockerJenkinsIntegrationApplication.java
@@ -2,10 +2,18 @@ package com.dedalus.spring.apis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerJenkinsIntegrationApplication {
 
+	@GetMapping("/message")
+	public void message() {
+		System.out.println("Welcome to Spring Application.");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
 	}
